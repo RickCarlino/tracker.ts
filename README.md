070151fab29da4946c3fc2a74b263fb785a1992f
Rick Carlino <rick.carlino@gmail.com>
2022-02-27 11:16:24 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tracker.TS
+
+This is a port of [Tracker.js from Meteor](https://github.com/meteor/meteor/blob/devel/packages/tracker/tracker.js) to Typescript.
+
+# Goals
+
+This is a learning exercise or me, as I am familiarizing myself with Meteor.JS.
+
+# Notable Changes
+
+ * References to Meteor have been removed. This project is intended to run as a standalone package.
+ * Minor stylistic cleanups, removal of doc comments that are redundant/obvious in typescript
+
+# TODO:
+
+ * Write tests for it to learn how it works.
--- tracker.ts
+++ tracker.ts
@@ -1,26 +1,195 @@
 type UNKNOWN_FUNC = () => void;
+
 type ComputationCallback = (c: Computation) => void;
+
 interface AutorunOpts { onError: UNKNOWN_FUNC; }
+
 interface FlushOptions {
   _throwFirstError: boolean;
   throwFirstError: boolean;
   finishSynchronously: boolean;
 }
+
 interface Tracker {
   active: boolean
   currentComputation: Computation | null;
-  _runFlush: Function;
+  _runFlush(opts: Partial<FlushOptions>): void;
   nonreactive(f: UNKNOWN_FUNC): void;
   flush(opts: FlushOptions): void;
   inFlush(): boolean;
-  autorun(f: UNKNOWN_FUNC, options: AutorunOpts): void;
+  autorun(f: UNKNOWN_FUNC, options: AutorunOpts): Computation;
   onInvalidate(f: UNKNOWN_FUNC): void;
   afterFlush(f: UNKNOWN_FUNC): void;
 }
 
-const Tracker = {
+const Tracker: Tracker = {
   active: false,
-} as Tracker;
+  currentComputation: null,
+  // http://docs.meteor.com/#tracker_flush
+  /**
+   * Process all reactive updates immediately and ensure that all invalidated computations are rerun.
+   */
+  flush(options: FlushOptions) {
+    Tracker._runFlush({
+      finishSynchronously: true,
+      throwFirstError: options && options._throwFirstError
+    });
+  },
+  /** True if we are computing a computation now, either first time
+   *  or recompute. This matches Tracker.active unless we are inside Tracker.nonreactive, which nullfies currentComputation even though an enclosing computation may still be running.
+   */
+  inFlush() {
+    return inFlush;
+  },
+  // Run all pending computations and afterFlush callbacks. If we were not called
+  // directly via Tracker.flush, this may return before they're all done to allow
+  // the event loop to run a little before continuing.
+  _runFlush(options?: Partial<FlushOptions>) {
+    if (Tracker.inFlush()) {
+      throw new Error("Can't call Tracker.flush while flushing");
+    }
+
+    if (inCompute) {
+      throw new Error("Can't flush inside Tracker.autorun");
+    }
+
+    options = options || {};
+
+    inFlush = true;
+    willFlush = true;
+    throwFirstError = !!options.throwFirstError;
+
+    let recomputedCount = 0;
+    let finishedTry = false;
+    try {
+      while (pendingComputations.length ||
+        afterFlushCallbacks.length) {
+
+        // recompute all pending computations
+        while (pendingComputations.length) {
+          const comp = pendingComputations.shift();
+          if (!comp) {
+            throw new Error("I should write a runtime assertion helper");
+          }
+          comp._recompute();
+          if (comp._needsRecompute()) {
+            pendingComputations.unshift(comp);
+          }
+
+          if (!options.finishSynchronously && ++recomputedCount > 1000) {
+            finishedTry = true;
+            return;
+          }
+        }
+
+        if (afterFlushCallbacks.length) {
+          // call one afterFlush callback, which may
+          // invalidate more computations
+          const func = afterFlushCallbacks.shift();
+          try {
+            func && func();
+          } catch (e) {
+            _throwOrLog("afterFlush", e as Error);
+          }
+        }
+      }
+      finishedTry = true;
+    } finally {
+      if (!finishedTry) {
+        // we're erroring due to throwFirstError being true.
+        inFlush = false; // needed before calling `Tracker.flush()` again
+        // finish flushing
+        Tracker._runFlush({
+          finishSynchronously: !!options.finishSynchronously,
+          throwFirstError: false
+        });
+      }
+      willFlush = false;
+      inFlush = false;
+      if (pendingComputations.length || afterFlushCallbacks.length) {
+        // We're yielding because we ran a bunch of computations and we aren't
+        // required to finish synchronously, so we'd like to give the event loop a
+        // chance. We should flush again soon.
+        if (options.finishSynchronously) {
+          throw new Error("still have more to do?");  // shouldn't happen
+        }
+        setTimeout(requireFlush, 10);
+      }
+    }
+  },
+  // http://docs.meteor.com/#tracker_autorun
+  //
+  // Run f(). Record its dependencies. Rerun it whenever the
+  // dependencies change.
+  //
+  // Returns a new Computation, which is also passed to f.
+  //
+  // Links the computation to the current computation
+  // so that it is stopped if the current computation is invalidated.
+  /**
+   * Run a function now and rerun it later whenever its dependencies
+   * change. Returns a Computation object that can be used to stop or observe the
+   * rerunning.
+   * one argument: the Computation object that will be returned.
+   * happens in the Computation. The only argument it receives is the Error
+   * thrown. Defaults to the error being logged to the console.
+   */
+  autorun(f: UNKNOWN_FUNC, options: AutorunOpts) {
+    options = options;
+
+    constructingComputation = true;
+    const c = new Computation(f, Tracker.currentComputation, options.onError);
+
+    if (Tracker.active) {
+      Tracker.onInvalidate(function () {
+        c.stop();
+      });
+    }
+
+    return c;
+  },
+
+  // http://docs.meteor.com/#tracker_nonreactive
+  //
+  // Run `f` with no current computation, returning the return value
+  // of `f`. Used to turn off reactivity for the duration of `f`,
+  // so that reactive data sources accessed by `f` will not result in any
+  // computations being invalidated.
+  /**
+   * Run a function without tracking dependencies.
+   */
+  nonreactive(f) {
+    const previous = Tracker.currentComputation;
+    setCurrentComputation(null);
+    try {
+      return f();
+    } finally {
+      setCurrentComputation(previous);
+    }
+  },
+
+  // http://docs.meteor.com/#tracker_oninvalidate
+  /**
+   * Registers a new [`onInvalidate`](#computation_oninvalidate) callback on the current computation (which must exist), to be called immediately when the current computation is invalidated or stopped.
+   */
+  onInvalidate(f) {
+    if (!Tracker.active || !Tracker.currentComputation) {
+      throw new Error("Tracker.onInvalidate requires a currentComputation");
+    }
+    Tracker.currentComputation.onInvalidate(f);
+  },
+
+  // http://docs.meteor.com/#tracker_afterflush
+  /** Schedules a function to be called during the next flush, or
+   *  later in the current flush if one is in progress, after all
+   *  invalidated computations have been rerun. The function will
+   *  be run once and not on subsequent flushes unless `afterFlush`
+   *  is called again. */
+  afterFlush(f) {
+    afterFlushCallbacks.push(f);
+    requireFlush();
+  }
+};
 
 function setCurrentComputation(c: Computation | null) {
   Tracker.currentComputation = c;
@@ -34,10 +203,8 @@ function _throwOrLog(from: string, e: Error) {
     const printArgs = ["Exception from Tracker " + from + " function:"];
     if (e.stack && e.message && e.name) {
       const idx = e.stack.indexOf(e.message);
-      if (idx < 0 || idx > e.name.length + 2) { // check for "Error: "
-        // message is not part of the stack
-        const message = e.name + ": " + e.message;
-        printArgs.push(message);
+      if (idx < 0 || idx > e.name.length + 2) {
+        printArgs.push(e.name + ": " + e.message);
       }
     }
     printArgs.push(e.stack ?? "NO STACK");
@@ -72,7 +239,7 @@ const afterFlushCallbacks: (UNKNOWN_FUNC)[] = [];
 
 function requireFlush() {
   if (!willFlush) {
-    setTimeout(Tracker._runFlush || (() => { }), 0);
+    setTimeout(Tracker._runFlush, 0);
     willFlush = true;
   }
 }
@@ -101,6 +268,7 @@ class Computation {
   private invalidated: boolean;
   private stopped: boolean;
   public _id: number;
+  public parent: Computation | null;
 
   constructor(f: ComputationCallback, parent: Computation | null, onError: UNKNOWN_FUNC) {
     if (!constructingComputation)
@@ -117,7 +285,7 @@ class Computation {
     /** True if this computation has been invalidated (and not
      *  yet rerun), or if it has been stopped.*/
     this.invalidated = false;
-
+    this.parent = parent;
     // http://docs.meteor.com/#computation_firstrun
 
     /** True during the initial run of the computation at the time
@@ -339,193 +507,10 @@ class Dependency {
   }
 
   // http://docs.meteor.com/#dependency_hasdependents
-
   /** True if this Dependency has one or more dependent Computations,
    *  which would be invalidated if this Dependency were to change. */
   hasDependents() {
-    for (const id in this._dependentsById)
-      return true;
+    for (const _id in this._dependentsById) { return true; }
     return false;
   }
-};
-
-// http://docs.meteor.com/#tracker_flush
-
-/**
- * Process all reactive updates immediately and ensure that all invalidated computations are rerun.
- */
-Tracker.flush = function (options: FlushOptions) {
-  Tracker._runFlush({
-    finishSynchronously: true,
-    throwFirstError: options && options._throwFirstError
-  });
-};
-
-/** True if we are computing a computation now, either first time
- *  or recompute. This matches Tracker.active unless we are inside Tracker.nonreactive, which nullfies currentComputation even though an enclosing computation may still be running.
- */
-Tracker.inFlush = function () {
-  return inFlush;
-}
-
-// Run all pending computations and afterFlush callbacks. If we were not called
-// directly via Tracker.flush, this may return before they're all done to allow
-// the event loop to run a little before continuing.
-Tracker._runFlush = function (options?: Partial<FlushOptions>) {
-  // XXX What part of the comment below is still true? (We no longer
-  // have Spark)
-  //
-  // Nested flush could plausibly happen if, say, a flush causes
-  // DOM mutation, which causes a "blur" event, which runs an
-  // app event handler that calls Tracker.flush. At the moment
-  // Spark blocks event handlers during DOM mutation anyway,
-  // because the LiveRange tree isn't valid. And we don't have
-  // any useful notion of a nested flush.
-  //
-  // https://app.asana.com/0/159908330244/385138233856
-  if (Tracker.inFlush())
-    throw new Error("Can't call Tracker.flush while flushing");
-
-  if (inCompute)
-    throw new Error("Can't flush inside Tracker.autorun");
-
-  options = options || {};
-
-  inFlush = true;
-  willFlush = true;
-  throwFirstError = !!options.throwFirstError;
-
-  let recomputedCount = 0;
-  let finishedTry = false;
-  try {
-    while (pendingComputations.length ||
-      afterFlushCallbacks.length) {
-
-      // recompute all pending computations
-      while (pendingComputations.length) {
-        const comp = pendingComputations.shift();
-        if (!comp) {
-          throw new Error("I should write a runtime assertion helper");
-        }
-        comp._recompute();
-        if (comp._needsRecompute()) {
-          pendingComputations.unshift(comp);
-        }
-
-        if (!options.finishSynchronously && ++recomputedCount > 1000) {
-          finishedTry = true;
-          return;
-        }
-      }
-
-      if (afterFlushCallbacks.length) {
-        // call one afterFlush callback, which may
-        // invalidate more computations
-        const func = afterFlushCallbacks.shift();
-        try {
-          func && func();
-        } catch (e) {
-          _throwOrLog("afterFlush", e as Error);
-        }
-      }
-    }
-    finishedTry = true;
-  } finally {
-    if (!finishedTry) {
-      // we're erroring due to throwFirstError being true.
-      inFlush = false; // needed before calling `Tracker.flush()` again
-      // finish flushing
-      Tracker._runFlush({
-        finishSynchronously: options.finishSynchronously,
-        throwFirstError: false
-      });
-    }
-    willFlush = false;
-    inFlush = false;
-    if (pendingComputations.length || afterFlushCallbacks.length) {
-      // We're yielding because we ran a bunch of computations and we aren't
-      // required to finish synchronously, so we'd like to give the event loop a
-      // chance. We should flush again soon.
-      if (options.finishSynchronously) {
-        throw new Error("still have more to do?");  // shouldn't happen
-      }
-      setTimeout(requireFlush, 10);
-    }
-  }
-};
-
-// http://docs.meteor.com/#tracker_autorun
-//
-// Run f(). Record its dependencies. Rerun it whenever the
-// dependencies change.
-//
-// Returns a new Computation, which is also passed to f.
-//
-// Links the computation to the current computation
-// so that it is stopped if the current computation is invalidated.
-
-/**
- * Run a function now and rerun it later whenever its dependencies
- * change. Returns a Computation object that can be used to stop or observe the
- * rerunning.
- * one argument: the Computation object that will be returned.
- * happens in the Computation. The only argument it receives is the Error
- * thrown. Defaults to the error being logged to the console.
- */
-Tracker.autorun = function (f: UNKNOWN_FUNC, options: AutorunOpts) {
-  options = options;
-
-  constructingComputation = true;
-  const c = new Computation(f, Tracker.currentComputation, options.onError);
-
-  if (Tracker.active)
-    Tracker.onInvalidate(function () {
-      c.stop();
-    });
-
-  return c;
-};
-
-// http://docs.meteor.com/#tracker_nonreactive
-//
-// Run `f` with no current computation, returning the return value
-// of `f`. Used to turn off reactivity for the duration of `f`,
-// so that reactive data sources accessed by `f` will not result in any
-// computations being invalidated.
-
-/**
- * Run a function without tracking dependencies.
- */
-Tracker.nonreactive = function (f) {
-  const previous = Tracker.currentComputation;
-  setCurrentComputation(null);
-  try {
-    return f();
-  } finally {
-    setCurrentComputation(previous);
-  }
-};
-
-// http://docs.meteor.com/#tracker_oninvalidate
-
-/**
- * Registers a new [`onInvalidate`](#computation_oninvalidate) callback on the current computation (which must exist), to be called immediately when the current computation is invalidated or stopped.
- */
-Tracker.onInvalidate = function (f) {
-  if (!Tracker.active || !Tracker.currentComputation) {
-    throw new Error("Tracker.onInvalidate requires a currentComputation");
-  }
-
-  Tracker.currentComputation.onInvalidate(f);
-};
-
-// http://docs.meteor.com/#tracker_afterflush
-/** Schedules a function to be called during the next flush, or
- *  later in the current flush if one is in progress, after all
- *  invalidated computations have been rerun. The function will
- *  be run once and not on subsequent flushes unless `afterFlush`
- *  is called again. */
-Tracker.afterFlush = function (f) {
-  afterFlushCallbacks.push(f);
-  requireFlush();
 };
